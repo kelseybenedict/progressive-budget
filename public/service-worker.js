49cfd9908402dbca5c776b8068caf3e3f591ff27
Kelsey Benedict <kelseybenedict26@gmail.com>
2021-05-09 22:53:44 -0600

    created db.js, added cache to service-worker

---
+++ public/service-worker.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = "budget-tracker-cache";
+const DATA_CACHE_NAME = "data-cache";
+const FILES_TO_CACHE = [
+    "/",
+    "/index.html",
+    "/styles.css",
+    "/db.js",
+    "/index.js",
+    "/icons/icon-192X192.png",
+    "/icons/icon-512x512.png",
+    "/manifest.webmanifest"
+  ];
+  
